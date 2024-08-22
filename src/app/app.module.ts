5dd865abb90fa2b19b7e13276bd97529f93410f9
CETECOM <cetecom_conce@duoc.cl>
2024-08-22 10:15:34 -0400

    wqdsadsa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ContactoComponent } from './contacto/contacto.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 import { AyudaComponent } from './pages/ayuda/ayuda.component';
-import { NotofundComponent } from './pages/notofund/notofund.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactoComponent,
-    AyudaComponent,
-    NotofundComponent
+    AyudaComponent
   ],
   imports: [
     BrowserModule,
