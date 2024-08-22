d13a96488ed85d43695fa520bfba4215307ea8f4
CETECOM <cetecom_conce@duoc.cl>
2024-08-22 10:15:08 -0400

    dsadsa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { NotofundComponent } from './pages/notofund/notofund.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent,
+    AyudaComponent,
+    NotofundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/notofund/notofund.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notofund',
+  templateUrl: './notofund.component.html',
+  styleUrl: './notofund.component.css'
+})
+export class NotofundComponent {
+
+}
